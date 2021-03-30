b7561ed49b4f0a41831261019ba4fc98b1f7d3ba
Alexej <39061639+alexejklass@users.noreply.github.com>
2021-03-30 22:37:51 +0200

    new design

--- js/main.js
+++ js/main.js
@@ -1,74 +1,54 @@
 //Variables
-const headerText = 'Web Developer';
-const headerAnimation = document.querySelector('.header__animation');
-const main = document.querySelector('.main').getBoundingClientRect().top;
-const techs = document.querySelectorAll('.main__icons img');
+const main = document.querySelector('.main').getBoundingClientRect().top
+const techs = document.querySelectorAll('.main__icons img')
 document
   .querySelector('footer')
   .addEventListener('click', () =>
-    window.scrollTo({ top: 0, left: 0, behavior: 'smooth' })
-  );
+    window.scrollTo({top: 0, left: 0, behavior: 'smooth'})
+  )
 
-const chooseTech = (event) => {
-  const cubes = document.querySelectorAll('div.cube');
-  techs.forEach((value) => value.classList.remove('active'));
+const chooseTech = event => {
+  const cubes = document.querySelectorAll('div.cube')
+  techs.forEach(value => value.classList.remove('active'))
   const visibleCubes = document.querySelectorAll(
     'div.cube.' + event.target.className
-  );
+  )
 
-  cubes.forEach((value) => {
-    value.style.display = 'none';
-  });
-  visibleCubes.forEach((value) => {
-    value.style.display = 'block';
-  });
-  event.target.classList.add('active');
-};
+  cubes.forEach(value => {
+    value.style.display = 'none'
+  })
+  visibleCubes.forEach(value => {
+    value.style.display = 'block'
+  })
+  event.target.classList.add('active')
+}
 
-techs.forEach((value) => value.addEventListener('click', chooseTech));
+techs.forEach(value => value.addEventListener('click', chooseTech))
 
-const scroll = (event) => {
-  window.scrollTo({ top: main + 100, left: 0, behavior: 'smooth' });
-};
+const scroll = event => {
+  window.scrollTo({top: main + 80, left: 0, behavior: 'smooth'})
+}
 const headerDown = document
   .querySelector('.header__down--wrap')
-  .addEventListener('click', scroll);
+  .addEventListener('click', scroll)
 
-let timeoutAdd = (step) => {
-  setTimeout(() => {
-    headerAnimation.textContent = headerText.slice(0, step);
-  }, 300 * step);
-};
-let timeoutDelete = (step) => {
-  setTimeout(() => {
-    headerAnimation.textContent = headerText.slice(0, -step);
-  }, 300 * (headerText.length + step));
-};
-
-const headerLoop = () => {
-  for (let index = 0; index <= headerText.length; index++) {
-    timeoutAdd(index);
-    timeoutDelete(index);
-  }
-};
-headerLoop();
-setInterval(() => {
-  headerLoop();
-}, headerText.length * 300 * 2.2);
+const headerProjects = document
+  .querySelector('.header__projects')
+  .addEventListener('click', scroll)
 
 function UnCryptMailto(s) {
-  var n = 0;
-  var r = '';
+  var n = 0
+  var r = ''
   for (var i = 0; i < s.length; i++) {
-    n = s.charCodeAt(i);
+    n = s.charCodeAt(i)
     if (n >= 8364) {
-      n = 128;
+      n = 128
     }
-    r += String.fromCharCode(n - 1);
+    r += String.fromCharCode(n - 1)
   }
-  return r;
+  return r
 }
 
 function linkTo_UnCryptMailto(s) {
-  location.href = UnCryptMailto(s);
+  location.href = UnCryptMailto(s)
 }
